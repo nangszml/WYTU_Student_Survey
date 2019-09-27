ef549f3e5d9ca3acf35e7ca4dcac9dded160a681
Swe Zin <swezinmoe1996@gmail.com>
2019-09-27 21:55:53 +0630

    please help me check with survey result

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,6 @@ class Student < ApplicationRecord
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-	belongs_to :department
+	belongs_to :department, dependent: :destroy
 	has_many :survey_results
 end
